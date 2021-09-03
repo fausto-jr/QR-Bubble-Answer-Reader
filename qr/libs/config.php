65b3d147743ec98961cbfc05b1af860c99340535
fausto-jr <62676471+fausto-jr@users.noreply.github.com>
2021-09-03 18:42:26 -0300

    Add files via upload

---
+++ qr/libs/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('db_name', 'answers');
+
+/** MySQL database username */
+define('db_user', 'root');
+
+/** MySQL database password */
+define('db_password', '');
+
+/** MySQL hostname */
+define('db_host', 'localhost');
